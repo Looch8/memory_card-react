f47fc5a20742a5bcb8cb03d6a05e50f3ab750dd7
Luke Wheldale <lukewheldale@gmail.com>
2023-09-29 12:51:55 +0930

    feat: App card logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import { useState, useEffect } from "react";
 import Card from "./components/Card";
 import CardGrid from "./components/CardGrid";
 
 function App() {
+	// Define initial array of cards
+	const initialCards = [
+		{ id: 1, name: "card1" },
+		{ id: 2, name: "card2" },
+	];
+
+	const [cards, setCards] = useState(initialCards); // array of cards
+	const [currentScore, setCurrentScore] = useState(0); // keeping track of score
+	const [bestScore, setBestScore] = useState(0);
+
+	// Function to shuffle cards
+	const shuffleCards = () => {
+		// card shuffling logic
+
+		// Update the 'cards' state with the shuffled cards array
+		setCards();
+	};
+
+	useEffect(() => {
+		shuffleCards();
+	}, []);
+
+	// determine when cards are matched
 	return (
-		<>
-			<CardGrid /> <Card />
-		</>
+		<div className="App">
+			{/* Render game components */}
+			<h1> Overwatch Memory Card Game</h1>
+			<div>
+				<p>Current Score: {currentScore}</p>
+				<p>Best Score: {bestScore}</p>
+			</div>
+			<CardGrid cards={cards} />
+		</div>
 	);
 }
 
