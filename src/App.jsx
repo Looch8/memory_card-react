1f17f4139c07659a0211adbfe8e30c26d71dbde9
Luke Wheldale <lukewheldale@gmail.com>
2023-10-05 16:38:58 +1030

    feat: card re-render after one is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import CardImages from "./components/CardImages";
 
 function App() {
 	//function to handle card click
-	const handleCardClick = () => {
-		//update the isAllFlipped
-		const updatedCards = cards.map((card) => ({
-			...card,
-			isFlipped: !isAllFlipped,
-		}));
+	// const handleCardClick = () => {
+	// 	//update the isAllFlipped
+	// 	const updatedCards = cards.map((card) => ({
+	// 		...card,
+	// 		isFlipped: !isAllFlipped,
+	// 	}));
 
-		setCards(updatedCards);
-		setIsAllFlipped(!isAllFlipped);
-		//update the score
-		//update the best score
-		//update the cards
-	};
+	// 	setCards(updatedCards);
+	// 	setIsAllFlipped(!isAllFlipped);
+	// 	//update the score
+	// 	//update the best score
+	// 	//update the cards
+	// };
 	return (
 		<div className="App">
 			<CardImages />
--- src/components/CardImages.jsx
+++ src/components/CardImages.jsx
@@ -8,32 +8,65 @@ function CardImages() {
 	const [currentScore, setCurrentScore] = useState(0);
 	const [bestScore, setBestScore] = useState(0);
 	const [isAllFlipped, setIsAllFlipped] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+	const [clickedCards, setClickedCards] = useState([]); // Store clicked card IDs
 
 	useEffect(() => {
 		// Fetch random cartoon images when the component mounts
-		fetchRandomCartoon().then((cartoonCards) => {
-			const shuffledCards = shuffleArray(cartoonCards);
-			setCards(shuffledCards);
-		});
-	}, []);
+		fetchRandomCartoon()
+			.then((cartoonCards) => {
+				const shuffledCards = shuffleArray(cartoonCards);
+				setCards(shuffledCards);
+				setIsLoading(false); // Set loading to false when data is loaded
+			})
+			.catch((error) => {
+				console.error("Error fetching data: ", error);
+				setIsLoading(false); // Set loading to false even on error
+			});
+	}, []); // Empty dependency array to ensure it runs only once
 
-	const handleCardClick = () => {
-		const updatedCards = cards.map((card) => ({
-			...card,
-			isFlipped: !isAllFlipped,
-		}));
-		setCards(updatedCards);
-		setIsAllFlipped(!isAllFlipped);
+	const handleCardClick = (cardId) => {
+		if (!isAllFlipped && !clickedCards.includes(cardId)) {
+			// Update the clicked cards
+			const updatedClickedCards = [...clickedCards, cardId];
+			setClickedCards(updatedClickedCards);
+
+			// Flip the card back after a delay (2-3 seconds)
+			setTimeout(() => {
+				const updatedCards = cards.map((card) => ({
+					...card,
+					isFlipped: false,
+				}));
+
+				// Fetch new random cards and shuffle
+				fetchRandomCartoon()
+					.then((newCartoonCards) => {
+						const shuffledNewCards = shuffleArray(newCartoonCards);
+						setCards(shuffledNewCards);
+					})
+					.catch((error) => {
+						console.error("Error fetching new data: ", error);
+					});
+
+				setCards(updatedCards);
+				setIsAllFlipped(false);
+			}, 1000); // Adjust the delay time as needed
+		}
 	};
 
+	// Render the cards only when the data is loaded
 	return (
 		<div className="App">
 			<Header currentScore={currentScore} bestScore={bestScore} />
-			<CardGrid
-				cards={cards}
-				onCardClick={handleCardClick}
-				isAllFlipped={isAllFlipped}
-			/>
+			{isLoading ? (
+				<div>Loading...</div>
+			) : (
+				<CardGrid
+					cards={cards}
+					onCardClick={handleCardClick}
+					isAllFlipped={isAllFlipped}
+				/>
+			)}
 		</div>
 	);
 }
